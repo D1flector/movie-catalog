2b13a254278e060dd582479ed1990bb099606d0d
Dmitry <dima.kornakow@yandeex.ru>
2025-10-02 18:22:14 +0300

    feat: настроил store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "../services/api";
 
 export const store = configureStore({
   reducer: {
-    
-  }
-})
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMidleware) => getDefaultMidleware().concat(api.middleware)
+});
+
+setupListeners(store.dispatch)
