27fb6c30a44902a1bbf1a1833cd81656fb5b68b3
Dmitry <dima.kornakow@yandeex.ru>
2025-10-02 16:26:48 +0300

    feat: переделаны типы для фильмов и сериалов

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,11 +1,15 @@
 import type { Genre } from "./common";
 
-export interface Movie {
+export interface MovieListItem {
   id: number;
   title: string;
   poster_path: string | null;
   vote_average: number;
   release_date: string;
   overview: string | null;
+  genre_ids: number[];
+}
+
+export interface MovieDetails extends Omit<MovieListItem, 'genre_ids'> {
   genres: Genre[];
 }
--- src/types/tv.ts
+++ src/types/tv.ts
@@ -1,11 +1,15 @@
 import type { Genre } from "./common";
 
-export interface Tv {
+export interface TvListItem {
   id: number;
   name: string;
   poster_path: string | null;
   vote_average: number;
   first_air_date: string;
   overview: string | null;
+  genre_ids: number[];
+}
+
+export interface TvDetails extends Omit<TvListItem, 'genre_ids'> {
   genres: Genre[];
 }
