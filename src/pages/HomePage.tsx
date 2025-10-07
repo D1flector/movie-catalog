7167860e383281af27d9365153d2bdce76619b6f
Dmitry <dima.kornakow@yandeex.ru>
2025-10-07 21:10:45 +0300

    feat: реализована главная страница

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,42 @@
-
+import { Link } from 'react-router-dom';
+import { useGetPopularMoviesQuery } from '../services/api';
+import '../styles/HomePage.scss';
 
 const HomePage = () => {
-  return (
-    <div>Homepage</div>
-  )
-}
+  const { data: popularMovies, isLoading } = useGetPopularMoviesQuery(1);
 
-export default HomePage
+  const backgroundStyle = popularMovies?.results[0]?.backdrop_path
+    ? { backgroundImage: `url(https://image.tmdb.org/t/p/original${popularMovies.results[0].backdrop_path})` }
+    : {};
+
+  if (isLoading) {
+    return <div className="home-page-loader">Загрузка...</div>;
+  }
+
+  return (
+    <div className="home-page" style={backgroundStyle}>
+      <div className="home-page__shape-overlay"></div>
+      <div className="home-page__backdrop-overlay"></div>
+
+      <div className="home-page__content">
+        <h1 className="home-page__title">
+          Ваш гид в мире кино
+        </h1>
+        <p className="home-page__subtitle">
+          Находите, исследуйте и открывайте для себя тысячи фильмов и сериалов. Ваш следующий любимый фильм уже ждет вас.
+        </p>
+
+        <div className="home-page__actions">
+          <Link to="/movies" className="home-page__button home-page__button--primary">
+            К фильмам
+          </Link>
+          <Link to="/tv" className="home-page__button home-page__button--secondary">
+            К сериалам
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
